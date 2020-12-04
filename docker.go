6f69e5cdf659f47ad8cf7df24d7bc21847f0bdfc
Janos Pasztor <janos@pasztor.at>
2020-12-04 11:49:22 +0100

    Implemented retries

--- docker.go
+++ docker.go
@@ -1,16 +1,10 @@
 package dockerrun
 
 import (
-	"context"
 	"crypto/tls"
 	"crypto/x509"
-	"errors"
-	"io/ioutil"
 	"net/http"
-	"strings"
 
-	"github.com/docker/distribution/reference"
-	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
@@ -39,34 +33,4 @@ func (config Config) getDockerClient() (*client.Client, error) {
 	}
 
 	return cli, nil
-}
-
-func (config Config) pullImage(cli *client.Client, ctx context.Context) error {
-	image := config.Config.ContainerConfig.Image
-	_, err := reference.ParseNamed(config.Config.ContainerConfig.Image)
-	if err != nil {
-		if errors.Is(err, reference.ErrNameNotCanonical) {
-			if !strings.Contains(config.Config.ContainerConfig.Image, "/") {
-				image = "docker.io/library/" + image
-			} else {
-				image = "docker.io/" + image
-			}
-		} else {
-			return err
-		}
-	}
-
-	pullReader, err := cli.ImagePull(ctx, image, types.ImagePullOptions{})
-	if err != nil {
-		return err
-	}
-	_, err = ioutil.ReadAll(pullReader)
-	if err != nil {
-		return err
-	}
-	err = pullReader.Close()
-	if err != nil {
-		return err
-	}
-	return nil
 }
